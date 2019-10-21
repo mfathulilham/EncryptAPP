bd14a549582a99b61d5fb76e129ebd4b0c8bac89
mfathulilham <mfathulilhamm@gmail.com>
2019-10-21 17:25:16 +0800

    Encrypt 1

---
+++ app/src/main/java/com/example/encryptapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.encryptapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.encrypt:
+                    fragment = new EncryptFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout,fragment,fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.decrypt:
+                    fragment = new DecryptFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout,fragment,fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState==null){
+            navigation.setSelectedItemId(R.id.encrypt);
+        }
+    }
+}
